487d8a4820db6ae91b279f8d1488f57c629b30c3
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-06-12 16:09:34 -0500

    Create model selector table

--- tests/test_model_selector.py
+++ tests/test_model_selector.py
@@ -12,13 +12,13 @@ def test_create_model_selector_table():
         ensure_db(engine)
         init_engine(engine)
         model_groups = {
-            'great': ModelGroupFactory(model_type='myGreatClassifier'),
+            'stable': ModelGroupFactory(model_type='myStableClassifier'),
             'bad': ModelGroupFactory(model_type='myBadClassifier'),
             'spiky': ModelGroupFactory(model_type='mySpikeClassifier'),
         }
 
-        class GreatModelFactory(ModelFactory):
-            model_group_rel = model_groups['great']
+        class StableModelFactory(ModelFactory):
+            model_group_rel = model_groups['stable']
 
         class BadModelFactory(ModelFactory):
             model_group_rel = model_groups['bad']
@@ -27,9 +27,9 @@ def test_create_model_selector_table():
             model_group_rel = model_groups['spiky']
 
         models = {
-            'great_3y_ago': GreatModelFactory(train_end_time='2014-01-01'),
-            'great_2y_ago': GreatModelFactory(train_end_time='2015-01-01'),
-            'great_1y_ago': GreatModelFactory(train_end_time='2016-01-01'),
+            'stable_3y_ago': StableModelFactory(train_end_time='2014-01-01'),
+            'stable_2y_ago': StableModelFactory(train_end_time='2015-01-01'),
+            'stable_1y_ago': StableModelFactory(train_end_time='2016-01-01'),
             'bad_3y_ago': BadModelFactory(train_end_time='2014-01-01'),
             'bad_2y_ago': BadModelFactory(train_end_time='2015-01-01'),
             'bad_1y_ago': BadModelFactory(train_end_time='2016-01-01'),
@@ -50,18 +50,18 @@ def test_create_model_selector_table():
             parameter = '100_abs'
 
         _ = [
-            Precision100Factory(model_rel=models['great_3y_ago'], value=0.6),
-            Precision100Factory(model_rel=models['great_2y_ago'], value=0.57),
-            Precision100Factory(model_rel=models['great_1y_ago'], value=0.59),
+            Precision100Factory(model_rel=models['stable_3y_ago'], value=0.6),
+            Precision100Factory(model_rel=models['stable_2y_ago'], value=0.57),
+            Precision100Factory(model_rel=models['stable_1y_ago'], value=0.59),
             Precision100Factory(model_rel=models['bad_3y_ago'], value=0.4),
             Precision100Factory(model_rel=models['bad_2y_ago'], value=0.39),
             Precision100Factory(model_rel=models['bad_1y_ago'], value=0.43),
             Precision100Factory(model_rel=models['spiky_3y_ago'], value=0.8),
             Precision100Factory(model_rel=models['spiky_2y_ago'], value=0.4),
             Precision100Factory(model_rel=models['spiky_1y_ago'], value=0.4),
-            Recall100Factory(model_rel=models['great_3y_ago'], value=0.55),
-            Recall100Factory(model_rel=models['great_2y_ago'], value=0.56),
-            Recall100Factory(model_rel=models['great_1y_ago'], value=0.55),
+            Recall100Factory(model_rel=models['stable_3y_ago'], value=0.55),
+            Recall100Factory(model_rel=models['stable_2y_ago'], value=0.56),
+            Recall100Factory(model_rel=models['stable_1y_ago'], value=0.55),
             Recall100Factory(model_rel=models['bad_3y_ago'], value=0.35),
             Recall100Factory(model_rel=models['bad_2y_ago'], value=0.34),
             Recall100Factory(model_rel=models['bad_1y_ago'], value=0.36),
@@ -70,27 +70,31 @@ def test_create_model_selector_table():
             Recall100Factory(model_rel=models['spiky_1y_ago'], value=0.36),
         ]
         session.commit()
-        model_selector = ModelSelector(db_engine=engine)
-        metrics = ['precision@100_abs', 'recall@100_abs']
+        model_selector = ModelSelector(db_engine=engine, models_table='models')
+        metrics = [('precision@', '100_abs'), ('recall@', '100_abs')]
         model_group_ids = [mg.model_group_id for mg in model_groups.values()]
-        output_table = model_selector.create_table(model_group_ids, metrics)
+        output_table = 'select_models_new'
+        model_selector.create_and_populate_table(model_group_ids, metrics, output_table)
+
+        # get an ordered list of the models/groups for a particular metric/time
         query = '''
             select model_id, below_max, below_max_next_time
-            from {} where metric = '%s' and test_set = '%s'
-            order by below_max desc
+            from {} where metric = %s and parameter = %s and train_end_time = %s
+            order by below_max
         '''.format(output_table)
-        prec_3y_ago = engine.execute(query, ('precision@100_abs', '2014-01-01'))
+
+        prec_3y_ago = engine.execute(query, ('precision@', '100_abs', '2014-01-01'))
         assert [row for row in prec_3y_ago] == [
-            (models['spiky_3y_ago'], 0, 0.17),
-            (models['great_3y_ago'], 0.2, 0),
-            (models['bad_3y_ago'], 0.4, 0.21),
+            (models['spiky_3y_ago'].model_id, 0, 0.17),
+            (models['stable_3y_ago'].model_id, 0.2, 0),
+            (models['bad_3y_ago'].model_id, 0.4, 0.18),
 
         ]
 
-        recall_2y_ago = engine.execute(query, ('recall@100_abs', '2015-01-01'))
-        assert [row for row in recall_3y_ago] == [
-            (models['spiky_2y_ago'], 0, 0.2),
-            (models['great_2y_ago'], 0.24, 0),
-            (models['bad_2y_ago'], 0.46, 0.19),
+        recall_2y_ago = engine.execute(query, ('recall@', '100_abs', '2015-01-01'))
+        assert [row for row in recall_2y_ago] == [
+            (models['spiky_2y_ago'].model_id, 0, 0.19),
+            (models['stable_2y_ago'].model_id, 0.24, 0),
+            (models['bad_2y_ago'].model_id, 0.46, 0.19),
 
         ]
--- triage/model_selectors.py
+++ triage/model_selectors.py
@@ -1,2 +1,54 @@
 class ModelSelector(object):
-    pass
+    def __init__(self, db_engine, models_table):
+        self.db_engine = db_engine
+        self.models_table = models_table
+        
+
+    def _create_table(self, table_name):
+        self.db_engine.execute('''create table {} (
+            model_group_id int,
+            model_id int,
+            train_end_time timestamp,
+            metric text,
+            parameter text,
+            below_max float,
+            below_max_next_time float
+        )'''.format(table_name))
+
+    def _populate_table(self, model_group_ids, metrics, output_table):
+        for metric, param in metrics:
+            self.db_engine.execute('''
+                insert into {new_table}
+                WITH model_ranks AS (
+                  SELECT m.model_group_id, m.model_id, m.train_end_time, ev.value,
+                         row_number() OVER (PARTITION BY m.train_end_time ORDER BY ev.value DESC, RANDOM()) AS rank
+                  FROM results.evaluations ev
+                  JOIN results.{models_table} m USING(model_id)
+                  JOIN results.model_groups mg USING(model_group_id)
+                  WHERE ev.metric='{metric}' AND ev.parameter='{metric_param}'
+                        AND m.model_group_id IN ({model_group_ids})
+                ),
+                model_tols AS (
+                  SELECT train_end_time, model_group_id, model_id,
+                         rank,
+                         value,
+                         first_value(value) over (partition by train_end_time order by rank ASC) AS best_val
+                  FROM model_ranks
+                ),
+                current_best_vals as (
+                    SELECT model_group_id, model_id, train_end_time, '{metric}', '{metric_param}', best_val - value below_max
+                    FROM model_tols
+                )
+                select current_best_vals.*, first_value(below_max) over (partition by model_group_id order by train_end_time asc rows between 1 following and unbounded following) below_max_next_time
+                from current_best_vals
+            '''.format(
+                model_group_ids=','.join(map(str, model_group_ids)),
+                models_table=self.models_table,
+                metric=metric,
+                metric_param=param,
+                new_table=output_table
+            ))
+
+    def create_and_populate_table(self, model_group_ids, metrics, output_table):
+        self._create_table(output_table)
+        self._populate_table(model_group_ids, metrics, output_table)
