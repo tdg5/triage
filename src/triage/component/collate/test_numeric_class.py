4964ebc76371d517f24a72808be4659293aa641c
harnav <hareemnav@gmail.com>
2017-12-21 15:47:19 -0600

    need to define how data will be passed with more rigour

--- src/triage/component/collate/test_numeric_class.py
+++ src/triage/component/collate/test_numeric_class.py
@@ -1,7 +1,7 @@
 from jinja2 import Template
 
 class NumericAggregation(Aggregation):
-    def __init__(self, intervals, column_name, aggregates, groups, from_obj, dates, state_table, state_group=None, prefix=None, suffix=None, date_column=None, output_date_column=None, input_min_date=None):
+    def __init__(self, intervals,  aggregates, groups, from_obj, dates, state_table, state_group=None, prefix=None, suffix=None, date_column=None, output_date_column=None, input_min_date=None):
     
     self.intervals = intervals
 
@@ -15,9 +15,9 @@ class NumericAggregation(Aggregation):
                         suffix=suffix,
                         schema=schema)
 
-    def define_template(self, groups, intervals, functions, column_name, prefix):
+    def define_template(self, groups, intervals, functions, from_obj, prefix):
     """Defines the templates for numeric aggregation. This template is then passed to the database connection object where it is executed in the standard collate format."""
-    return """
+    template =  """
     select {{ group }}, {{ as_of_date }}::date as date
     {% for interval in intervals %}
     {% for function in functions %}
@@ -25,14 +25,26 @@ class NumericAggregation(Aggregation):
     "{{ prefix }}_{{ group }}_{{ interval }}_{{ column_name }}"
     {% endfor %}
     {% endfor %}
-    FROM {{ table_name }}
+    FROM {{ from_obj }}
     WHERE date < {{ as_of_date }} AND date >= {{ as_of_date }}::date - greatest(
     {% for interval in intervals %}
     interval {{ interval }},
     {% endfor %} )
     GROUP BY {{ group }}"""
 
-    def render_template(self):
+    return template
+    
+    def define_data(self, groups, intervals, functions, from_obj, prefix, as_of_date):
+        pass
+
+    def render_template(self, template):
+    t = Template(template)
+    return t
+
+    def execute_db(self, t):
+    db_conn.execute(t.render(data))
+
+
 
 
   
